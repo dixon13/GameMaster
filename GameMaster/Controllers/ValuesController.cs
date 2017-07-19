0c2ad48c9376b429901cb951743f70f7f117af22
Dixon Begay <dixonbegay@gmail.com>
2017-07-18 21:57:43 -0700

    Added Swagger stuff

--- GameMaster/Controllers/ValuesController.cs
+++ GameMaster/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace GameMaster.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return $"value {id}";
         }
 
         // POST api/values
--- GameMaster/Startup.cs
+++ GameMaster/Startup.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace GameMaster
 {
@@ -29,6 +26,11 @@ namespace GameMaster
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +39,11 @@ namespace GameMaster
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseMvc();
         }
     }
