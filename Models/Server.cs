0f96da83f8c81763856274d54f6f26efde5e8ae5
Dixon Begay <dixonbegay@gmail.com>
2017-07-24 21:52:09 -0700

    started working on Server class

---
+++ Models/Server.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameMaster.Models
+{
+    public class Server
+    {
+        public int ServerID { get; set; }
+
+        public string ServerName { get; set; }
+
+        public string Game { get; set; }
+
+    }
+}
